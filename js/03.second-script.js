27a322a47a235a71f02f25f8808096f60b20b102
JustForEducate <117161265+JustForEducate@users.noreply.github.com>
2022-11-01 13:25:26 +0300

    Add files via upload

---
+++ js/03.second-script.js
@@ -0,0 +1,14 @@
+/*
+А этот сценарий выведет текст в консоль отладчика
+*/
+
+// Заглушка для старых браузеров
+if (!window.console || !console.firebug) {
+    var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml", "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
+    window.console = {};
+    for (var i = 0; i < names.length; ++i) {
+        window.console[names[i]] = function() {}
+    }
+}
+
+console.log("И этот скрипт тоже работает");
